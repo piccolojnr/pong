fe28dbbf0f8c1a1a93a9800878add3ad578a6325
Rahim Daud <rahimdaud246@gmail.com>
2022-10-12 23:40:00 +0000

    pong-1

--- main.lua
+++ main.lua
@@ -1,10 +1,22 @@
+push = require "push"
+
+
 WINDOW_HEIGHT = 720
 WINDOW_WIDTH = 1280
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
 
 -- USED TO  INITIALIZE GAME
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- use nearest-neighbor filtering on upscaling and downscaling
+    -- to prevent blurring of text
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- initialize our virtual resolution, which will be rendered within
+    -- our actual window no matter its dimensions
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -12,7 +24,24 @@ function love.load()
 
 end
 
+-- KEYBOARD HANDLING
+function love.keypressed(key)
+    if key == 'escape' then
+        -- function to terminate application
+        love.event.quit()
+    end
+end
+
 --  USED TO DRAW ANYTHING TO THE SCREEN
 function love.draw()
-    love.graphics.printf('Hello Pong!', 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
+    -- begin rendering at virtual resolution
+    push:apply('start')
+
+
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
+
+
 end
